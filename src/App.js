249c0a077b5dbf7e99aafd855f6a2c9bcd2fa84e
mdemon420 <mdeamon4@gmail.com>
2022-07-01 20:10:44 +0600

    from

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h3>Please Register</h3>
+      <from>
+        <label htmlFor='email'>Email : </label>
+        <input type="text" name="email" placeholder="enter your email" />
+        <br />
+        <br />
+        <label htmlFor='password'>Password : </label>
+        <input type="password" name="password" placeholder="password"></input>
+      </from>
     </div>
   );
 }
