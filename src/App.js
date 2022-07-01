503ba41a276cfe29e86cd92226aafbfd76227a1b
mdemon420 <mdeamon4@gmail.com>
2022-07-01 20:32:30 +0600

    sumnit button added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 
 import './App.css';
+import Form from './Components/Form/Form';
 
 function App() {
+
+
   return (
     <div className="App">
-      <h3>Please Register</h3>
-      <from>
-        <label htmlFor='email'>Email : </label>
-        <input type="text" name="email" placeholder="enter your email" />
-        <br />
-        <br />
-        <label htmlFor='password'>Password : </label>
-        <input type="password" name="password" placeholder="password"></input>
-      </from>
+      <Form></Form>
     </div>
   );
 }
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Form = () => {
+    const handleRegistration = event => {
+        console.log("submit")
+        event.preventDefault()
+    }
+    return (
+        <div>
+            <h3>Please Register</h3>
+            <form onSubmit={handleRegistration}>
+                <label htmlFor='email'>Email : </label>
+                <input type="text" name="email" placeholder="enter your email" />
+                <br />
+                <br />
+                <label htmlFor='password'>Password : </label>
+                <input type="password" name="password" placeholder="password"></input>
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Form;
